d323a2c589960dce4a143a901ce3ea2eb93b79e6
amazoradi <amazoradi@gmail.com>
2018-10-18 15:59:03 -0500

    js file added, linked to html, intilal js additions

---
+++ src/scripts/journal.js
@@ -0,0 +1,8 @@
+const journalEntries = [
+  {
+    date: "10/18/2018",
+    concept: "API",
+    entry: "We looked at how to look at and get, use and change information from big JSON databases",
+    mood: "overwhelmed"
+  }
+]
