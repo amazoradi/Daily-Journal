8f20a5fd470543a63b5d12e08b14262f198747ab
amazoradi <amazoradi@gmail.com>
2018-10-19 13:53:23 -0500

    part two complete, with two functions (one to make entries and one to put them on the DOM)

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -7,7 +7,7 @@ const journalEntries = [
   },
   {
     date: "10/18/2018",
-    concept: "API",
+    concept: "Intro to API",
     entry: "We looked at how to look at and get, use and change information from big JSON databases",
     mood: "overwhelmed"
   },
@@ -20,21 +20,37 @@ const journalEntries = [
 ];
 
 
+let fragment = document.createDocumentFragment();
+let entryArt = document.querySelector(".entryLog");
 
-//  let fragment = document.createDocumentFragment();
 
-
-journalEntries.forEach(element => {
-  let entryArt = document.querySelector(".entryLog");
+// this function creates a journal entry structure; the h2 and p tags and also says what, from our object, should go in each tag
+const makeJournalEntries = (journalObject) => {
   let entryHolder = document.createElement("section");
-  entryArt.appendChild(entryHolder)
   let entryTitle = document.createElement("h2");
+  entryTitle.textContent = journalObject.concept
   let entryMeat = document.createElement("p")
-  entryTitle.textContent = element.concept
-  entryMeat.textContent = element.entry + " " + element.date
+  entryMeat.textContent = `${journalObject.entry} I am ${journalObject.mood}. ${journalObject.date}`
   entryHolder.appendChild(entryTitle)
   entryHolder.appendChild(entryMeat)
-});
+  return entryHolder
+}
+// This function calls the function creating an entry structure and fills it with the object at position [i] of our entry arry
+// it then puts that populated entry in a fragment until it has gone through the entire array and puts the fragment on the DOm once called.
+const renderedEntries = (array) => {
+  for (let i =0; i < journalEntries.length; i++){
+    let newEntry = makeJournalEntries(array[i]);
+    fragment.appendChild(newEntry)
+  } entryArt.appendChild(fragment)
+}
+
+renderedEntries(journalEntries)
+
+
+
+
+  
+
 
 
 
