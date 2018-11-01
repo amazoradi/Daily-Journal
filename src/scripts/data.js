6df90700c4884f42170cc4a882b5a082d9abf6de
amazoradi <amazoradi@gmail.com>
2018-10-31 22:06:47 -0500

    deleted commented out code, added comments

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,15 +1,4 @@
-// getting our data from the database
-// fetch(" http://localhost:8088/journalEntries")
-//   // convert or database data to javascript
-//   .then(entry => entry.json())
-//   .then(parsedEntry => {
-//     // loop over the array of fetched data
-//     parsedEntry.forEach((entry) => {
-//       // creates DOM elements by calling our function
-//       domCreation(entry)
-//     })
-//   })
-
+// creating an objec that is fetching our data
 const API = {
   getJournalEntries() {
     return fetch("http://localhost:8088/journalEntries")
