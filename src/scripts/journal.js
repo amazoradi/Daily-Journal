48ff6e5ecf6fda835dc47071fdc08e0a91e1b138
amazoradi <amazoradi@gmail.com>
2018-11-02 16:03:45 -0500

    added grunt, almost completed dom inject along with database injection

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -6,28 +6,63 @@ API.getJournalEntries().then(entries => putOnDOM.domCreation(entries))
 // add a click event listener to the Record Journal Entry button at the bottom of your form. When the user clicks the button, you need to create a new entry in your API. The HTTP method that you use to create resources is POST. Guidance on syntax is provided below.
 
 let recordButton = document.getElementById("record-button")
+let dateJournal = document.getElementById("journalDate")
+let conceptJournal =  document.getElementById("conceptsCovered")
+let entryJournal = document.getElementById("journalEntry")
+let moodJournal = document.getElementById("mood")
+let counter = 4
 
 function createEvent () {
 recordButton.addEventListener("click", () => {
   console.log("click")
   let entryObject = {
-    "date": document.getElementById("journalDate").value,
-    "concept": document.getElementById("conceptsCovered").value,
-    "entry": document.getElementById("journalEntry").value,
-    "mood": document.getElementById("mood").value
+    "date": dateJournal.value,
+    "concept": conceptJournal.value,
+    "entry": entryJournal.value,
+    "mood": moodJournal.value,
+    "id": counter
   }
   console.log(entryObject)
-//  saveToEntries(entryObject)
+  //add function to apply logic 
+  validateEntry()
+  saveToEntries(entryObject)
+  counter += 1
 })}
 
 createEvent()
 
-// const saveToEntries = (data) => {
-//   return fetch("http://localhost:8088/journalEntries", {
-//     method: "PUT",
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(data)
-//   })
-// }
+
+  const saveToEntries = (data) => {
+    return fetch(`http://localhost:8088/journalEntries/${counter}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+}
+
+
+
+function validateEntry () {
+ 
+  if (dateJournal.value === ""){
+    console.log("Hold up")
+    alert("Please fill out all sections")
+  } 
+  if (conceptJournal.value === "") {
+    console.log("Hold up")
+    alert("Please fill out all sections")
+}
+  if (entryJournal.value === "") {
+    console.log("Hold up")
+    alert("Please fill out all sections")
+  } 
+  if (moodJournal.value === "") {
+    console.log("Hold up")
+    alert("Please fill out all sections")
+  }
+}
+// Using required attribute to ensure no blank entries
+// No characters other than letters, numbers, (), {}, :, and;
+// let input = (document.queryselector("#input id")) if input.hasattribute('required') && input.value === ""{dont sumbit form} else {submit form}
