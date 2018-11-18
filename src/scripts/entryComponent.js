8c82ac537c0453b26ceea1758df0ace35e13fc3f
amazoradi <amazoradi@gmail.com>
2018-11-18 13:57:14 -0600

    added styling

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -6,9 +6,11 @@ let entryArt = document.querySelector(".entryLog");
 
 const putOnDOM = {
   postNewEntry(entry) {
-    let entryTitle = elementCreator.elementFactory("h2", entry.concept)
-    let entryContent = elementCreator.elementFactory("p", `${entry.entry} I am ${entry.mood.label}. The instructor today was ${entry.instructor.firstName} ${entry.instructor.lastName}. ${entry.date}`)
-    let entryHolder = elementCreator.elementFactory("section", null, "entrySection", `${entry.id}`, entryTitle, entryContent)
+    let entryTitle = elementCreator.elementFactory("h2", entry.concept, "entryTitle")
+    let entryDate = elementCreator.elementFactory("h3", `${entry.date}`, "entryDate")
+    let entryMood = elementCreator.elementFactory("h4", `My mood: ${entry.mood.label}.`, "entryMood")
+    let entryContent = elementCreator.elementFactory("p", `${entry.entry}. The instructor today was ${entry.instructor.firstName} ${entry.instructor.lastName}.`)
+    let entryHolder = elementCreator.elementFactory("section", null, "entrySection", `${entry.id}`, entryTitle, entryDate, entryMood, entryContent)
     //appending our new elements to the fragment then the fragment to or article  
     let fragment = document.createDocumentFragment()
     fragment.appendChild(entryHolder)
